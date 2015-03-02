268fb87fe01804df545d435f740ae98f2a90502d
Samuel <Samuel@Samuels-Macbook-Pro.local>
2015-03-02 14:17:29 -0300

    Projeto criado

---
+++ FuelLine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FuelLine
+//
+//  Created by Samuel Shin Kim on 02/03/15.
+//  Copyright (c) 2015 Samuel Shin Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FuelLine/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FuelLine
+//
+//  Created by Samuel Shin Kim on 02/03/15.
+//  Copyright (c) 2015 Samuel Shin Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
